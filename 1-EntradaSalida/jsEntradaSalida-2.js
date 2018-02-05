fabcac53720ea3e79c0da72bae51cb62f9db4107
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-05 20:54:42 -0300

    Ejercicio 1 y2
    ver el alert, promp, variables y concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,20 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+
+
+
 function Mostar()
 {
-	
+
+    //guardar con crl + s
+	//alert("anda");
+	var nombre;
+/*	alert(nombre);
+	nombre="Giuliano";
+	alert(nombre);
+*/
+	nombre = prompt("ingrese su nombre", "ej:Giuliano");
+	//alert(nombre);
+	alert("su nombre es: " + nombre);
 }
 
