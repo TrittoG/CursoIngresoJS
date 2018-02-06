09cbaaeca11771c12ad724806ac94da4eb1cb790
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-06 17:12:16 -0300

    ejercicios 6 y 7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numero1;
+	var numero2;
+
+	numero1 = parseInt(document.getElementById('numeroUno').value);
+	numero2 = parseInt(document.getElementById('numeroDos').value);
+
+	alert("la suma es: " +(numero1+numero2));
+
+
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,49 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numero1;
+	var numero2;
+
+		numero1 = parseInt(document.getElementById('numeroUno').value);
+		numero2 = parseInt(document.getElementById('numeroDos').value);
+
+		alert("la suma es: " +(numero1+numero2));
+
 }
 
 function restar()
 {
+	var numero1;
+	var numero2;
+
+		numero1 = parseInt(document.getElementById('numeroUno').value);
+		numero2 = parseInt(document.getElementById('numeroDos').value);
+
+		alert("la resta es: " +(numero1-numero2));
 	
 }
 
 function multiplicar()
 { 
+	var numero1;
+	var numero2;
+
+		numero1 = parseInt(document.getElementById('numeroUno').value);
+		numero2 = parseInt(document.getElementById('numeroDos').value);
+
+		alert("la multiplicacion es: " +(numero1*numero2));
 	
 }
 
 function dividir()
 {
-	
+	var numero1;
+	var numero2;
+
+		numero1 = parseInt(document.getElementById('numeroUno').value);
+		numero2 = parseInt(document.getElementById('numeroDos').value);
+
+		alert("la division es: " +(numero1/numero2));
+
 }
 
