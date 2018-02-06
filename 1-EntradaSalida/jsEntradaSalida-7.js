1542a247b1732279988d5930fe5f7656766ac059
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-06 19:51:44 -0300

    ej 7 corregido y 8,9,10

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,49 +4,72 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var resultado;
 	var numero1;
 	var numero2;
 
-		numero1 = parseInt(document.getElementById('numeroUno').value);
-		numero2 = parseInt(document.getElementById('numeroDos').value);
+		numero1 = document.getElementById('numeroUno').value;
+		numero2 = document.getElementById('numeroDos').value;
 
-		alert("la suma es: " +(numero1+numero2));
+		numero1 =parseInt(numero1);
+		numero2 = parseInt(numero2);
+
+		resultado = numero1+numero2;
+
+
+		alert("la suma es: " +resultado);
 
 }
 
 function restar()
 {
+
+	var resultado;
 	var numero1;
 	var numero2;
 
-		numero1 = parseInt(document.getElementById('numeroUno').value);
-		numero2 = parseInt(document.getElementById('numeroDos').value);
+		numero1 = document.getElementById('numeroUno').value;
+		numero2 = document.getElementById('numeroDos').value;
 
-		alert("la resta es: " +(numero1-numero2));
+		
+
+		resultado= numero1-numero2;
+
+		alert("la resta es: " +resultado);
 	
 }
 
 function multiplicar()
 { 
+	var resultado;
 	var numero1;
 	var numero2;
 
-		numero1 = parseInt(document.getElementById('numeroUno').value);
-		numero2 = parseInt(document.getElementById('numeroDos').value);
+		numero1 = document.getElementById('numeroUno').value;
+		numero2 = document.getElementById('numeroDos').value;
 
-		alert("la multiplicacion es: " +(numero1*numero2));
+		
+
+		resultado = numero1*numero2;
+
+		alert("la multiplicacion es: " +resultado);
 	
 }
 
 function dividir()
 {
+	var resultado;
 	var numero1;
 	var numero2;
 
-		numero1 = parseInt(document.getElementById('numeroUno').value);
-		numero2 = parseInt(document.getElementById('numeroDos').value);
+		numero1 = document.getElementById('numeroUno').value;
+		numero2 = document.getElementById('numeroDos').value;
 
-		alert("la division es: " +(numero1/numero2));
+
+
+		resultado = numero1/numero2;
+
+		alert("la division es: " +resultado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,24 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+		
+		var resultado;
+		var dividendo;
+		var divisor;
+
+		dividendo = document.getElementById('numeroDividendo').value;
+		divisor = document.getElementById('numeroDivisor').value;
+
+		dividendo = parseInt(dividendo);
+		divisor = parseInt(divisor);
+
+		resultado = dividendo % divisor;
+
+		alert(resultado);
+		
+
+
+
+
+
 }
