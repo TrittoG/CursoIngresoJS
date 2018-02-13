39e643ecf87343f6767207db651cb960e2b26c7a
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-13 01:10:07 -0300

    Ejercicios 8,9 y 10
    sentencia if y random.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,23 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var nota;
 
+	nota = (Math.random()*10)+1;
+
+	nota = parseInt(nota);
+
+	alert(nota);
+
+	if(nota >= 9){
+
+		alert("Excelente");
+
+	}else
+	{
+		if(nota>=4){
+			alert("Aprobo");
+		}else
+			alert("DESAPROBADO");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+
+	var random;
+
+	random = (Math.random()*10)+1;
+
+	//el random genera un numero del 0 al 1, lo multiplico por 10 y le sumo 1 para que me den numeros del 1 al 10.
+
+	random = parseInt(random);
+
+	//aca le saque los decimales
+
+	alert(random);
 	
+
+
 
 }//FIN DE LA FUNCIÓN
