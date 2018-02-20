7595d726b371cdf9d29c97fb4daa8de3a6eb9838
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-19 22:13:43 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,9 +11,20 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
-
+var intentos;
+var numero;
 function comenzar()
-{
+{	
+	
+	numeroSecreto = Math.floor((Math.random()*100)+1);
+
+	console.log(numeroSecreto);
+
+	intentos = 0;
+
+	document.getElementById('intentos').value = intentos;
+
+	
 	//Genero el número RANDOM entre 1 y 100
 	 
 	
@@ -21,8 +32,81 @@ function comenzar()
 }
 
 function verificar()
-{
+{	
+	intentos = intentos + 1;
+
+	document.getElementById('intentos').value = intentos;
+	numero = document.getElementById('numero').value ;
+	numero = parseInt(numero);
+
+	switch(intentos)
+	{
+		case 1 :
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("usted es Psíquico");
+			}
+		}break;
+
+		case 2 :
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("excelente");
+			}
+		}break;
+
+		case 3 :
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("esto es suerte");
+			}
+		}break;
+
+		case 4 :
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("excelente tecnica");
+			}
+		}break;
+
+
+		case 5 :
+		{
+			if(numeroSecreto == numero)
+			{
+				alert("usted esta en la media");
+			}
+		}break;
+
+		case 6 :
+		case 7 :
+		case 8 :
+		case 9 :
+		case 10:
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("falta tecnica");
+			}
+		}break;
+
+		default :
+		{
+			if(numero == numeroSecreto)
+			{
+				alert("bin en el amor");
+			}
+		}break;
+
+
+
+	}	
+
 	
-	
+
 
 }
--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -8,22 +8,94 @@ var eleccionMaquina;
 
 function comenzar()
 {
-	
+	eleccionMaquina = Math.Floor((Math.random()*3)+1);
+
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
-{
-	
+{	
+	var numero;
+
+	numero = 1;
+		
+	switch(eleccionMaquina)
+	{
+		case 1 :
+		{
+			alert("empato");
+		}break;
+		
+		case 2 :
+		{
+			alert("perdiste");
+		}break;
+
+		case 3 :
+		{
+			alert("ganaste");
+		}break;
+
+
+
+	}
+
+
 
 }//FIN DE LA FUNCIÓN
 function papel()
-{
+{	
+	var papel;
+	papel = 2;
+
+	switch(eleccionMaquina)
+	{
+		case 1 :
+		{
+			alert("ganaste");
+		}break;
+		
+		case 2 :
+		{
+			alert("empataste");
+		}break;
+
+		case 3 :
+		{
+			alert("perdiste");
+		}break;
+
+
+
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
+
+	var numero;
+	numero = 3;
+
+	switch(eleccionMaquina)
+	{
+		case 1 :
+		{
+			alert("perdiste");
+		}break;
+		
+		case 2 :
+		{
+			alert("ganaste");
+		}break;
+
+		case 3 :
+		{
+			alert("empata");
+		}break;
 	
 
 }//FIN DE LA FUNCIÓN
