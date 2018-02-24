3350318f1eaed7d95445898c7cb7310b72ee90d7
TrittoG <36174147+TrittoG@users.noreply.github.com>
2018-02-24 12:58:08 -0300

    ejercicios 6 y 8

---
+++ 9-Parciales/ocho.js
@@ -0,0 +1,191 @@
+//ingreso de alumnos, no se sabe cuantos, pedir nombre cuantos alumnos ingrese? por document.write
+/* 
+cantidad de alumnos
+promedio total
+cantidad de mujeres
+cantidad de varones
+cantidad de varones desaprobados
+nombre de la mejor nota
+promedio de mujeres
+sexo de menor edad
+promedio de las edades de los aprobados
+cantidad de notas pares
+cantidad de notas impares*/
+
+function Mostrar()
+{	
+	var nombre;
+	var cantidad = 0;
+	var seguir="";	
+	var nota;
+	var acumuladorDeNotas = 0;
+	var mujer = 0;
+	var hombre = 0;
+	var sexo="";
+	var hombreDesaprobado = 0;
+	var maximaNota = 0;
+	var nombremaximo;
+	var promedioMujeres;
+	var notaMujeres = 0;
+	var edad;
+	var menorEdad = 0;
+	var sexomenoredad;
+	var edadAprobados = 0;
+	var promedioAprobados = 0;
+	var contadorAprobados = 0;
+	var notasPares;
+	var notasImpares;
+	var notasCeros;
+
+	while(seguir != "no")
+	{
+		nombre = prompt("ingrese nombre del alumno: ");
+		nota = prompt("ingrese la nota que se saco");
+		nota = parseInt(nota);
+		while(nota < 0 || nota > 10)
+		{
+			nota = prompt("error ingrese una nota valida");
+			nota = parseInt(nota);
+		}
+
+		if(nota % 2 == 0 && nota != 0)
+		{
+			notasPares++;
+		}
+		else
+		{
+			if(nota % 2 == 1)
+			{
+				notasImpares++;
+			}
+			else
+			{
+				notasCeros++;
+			}
+		}
+
+
+		sexo = prompt("ingrese M (mujer) o H (hombre)");
+		while(sexo != "m" && sexo != "h") //cantidad de varones y de mujeres
+		{
+			sexo = prompt("ingrese M (mujer) o H (hombre)");
+		}
+
+		if(sexo == "m")
+		{
+			mujer = mujer +1;	
+			notaMujeres = notaMujeres + nota; //promedio de mujeres
+
+		}
+		
+
+
+
+		if(sexo == "h") //cantidad de varones desarobados
+		{
+			if(nota < 4)
+			{
+				hombreDesaprobado = hombreDesaprobado +1;
+			}
+		}
+
+		edad = prompt("ingrese la edad");
+		edad = parseInt(edad);
+		while(edad < 0 || edad > 100)
+		{
+			edad = prompt("ingrese la edad correcta");
+		}
+
+
+
+
+		if(nota >= 4) //promedio de las edades de aprobados
+		{
+
+			
+			contadorAprobados = contadorAprobados + 1;
+			edadAprobados = edadAprobados + edad;
+
+		}
+
+
+
+
+
+		cantidad = cantidad +1;
+
+
+		if(cantidad == 1) // menor edad
+		{
+			menorEdad = edad;
+			sexomenoredad = sexo;
+		}
+		else
+		{
+			if(edad < menorEdad)
+			{
+				menorEdad = edad;
+				sexomenoredad = sexo;
+			}
+		}
+
+		
+
+
+		if(cantidad == 1)//nombre de la mejor nota
+		{
+			maximaNota = nota;
+			nombremaximo = nombre;
+		}
+		else
+		{
+			if(nota > maximaNota)
+			{
+				maximaNota = nota;
+				nombremaximo = nombre;
+			}
+		}
+
+
+
+		seguir = prompt("NO para salir ");
+
+		acumuladorDeNotas = (acumuladorDeNotas + nota);
+
+
+
+
+
+	}//fin del while
+
+
+	promedioAprobados = edadAprobados / contadorAprobados;
+
+	if(sexomenoredad == "m")
+	{
+		sexomenoredad = "Mujer";
+	}
+	else
+	{
+		sexomenoredad = "hombre";
+	}
+
+	document.write(" son " +cantidad +" alumnos ");
+
+	promedio = acumuladorDeNotas/cantidad
+	promedioMujeres = notaMujeres/mujer;
+
+	document.write("<br>el promedio total es: " +promedio);
+	document.write("<br> cantidad de hombres: " +hombre +" cantidad de mujeres: " +mujer);
+	document.write("<br> cantidad de varones que desaprobaron: " +hombreDesaprobado);
+	document.write("<br> el de la nota maxima es: " +nombremaximo);
+	document.write("<br>el promedio de las mujeres es: " +promedioMujeres);
+	document.write("<br> el sexo del de menor edad es: " +sexomenoredad);
+	document.write("<br> el promedio de las edades de aprobados es: " +promedioAprobados);
+	document.write("<br> la cantidad de notas pares es: " +notasPares +", la cantidad de notas impares es: " +notasImpares +" y la cantidad de notas 0 es: " +notasCeros );
+
+}
+
+
+
+
---
+++ 9-Parciales/seis.js
@@ -0,0 +1,35 @@
+function Mostrar()
+{
+	var venta;
+	var i;
+	var maximo;
+	var minimo;
+	var contador;
+
+
+
+
+	for(i = 0; i = 24; i++)
+	{
+		venta = prompt("ingrese importe de venta del dia");
+
+		while(venta < 0)
+		{
+			alert("ingrese importe de venta del dia");
+		}
+		
+
+		if(venta == 1)
+		{
+			minimo = venta;
+		}
+		
+
+
+
+	}
+
+	
+
+
+}
